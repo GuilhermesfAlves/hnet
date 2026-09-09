510349a4282927ae4faffde996738e6345eaea99
Guilherme <gsfa22@inf.ufpr.br>
2026-09-09 17:33:39 -0300

    [mod] move files to folders

--- slurm/train.sh
+++ slurm/train.sh
@@ -18,12 +18,10 @@ NVIDIA_SMI_PID=$!
 hnet_names=("hnet_1stage_L" "hnet_1stage_XL" "hnet_2stage_L" "hnet_2stage_XL")
 batch_sizes=(12 10 8 8)
 sequence_lengths=(768 768 768 768)
-# learning_rates=(6.25e-4 5e-4 6.25e-4 5e-4)
 
 for i in "${!hnet_names[@]}";do
 	hnet="${hnet_names[$i]}"
 	batch="${batch_sizes[$i]}"
-	# lr="${learning_rates[$i]}"	
 	seq_len="${sequence_lengths[$i]}"
 
 	echo "$hnet $batch $seq_len"
@@ -31,11 +29,11 @@ for i in "${!hnet_names[@]}";do
 	srun --export=ALL python -m torch.distributed.run --nproc_per_node=4 train_pt.py \
         	--dataset-name uonlp/CulturaX \
 	        --dataset-config-name pt \
-		--csv-path checkpoints/train_pt/$hnet.metrics.txt \
+			--csv-path checkpoints/train_pt/$hnet.metrics.txt \
 	        --text-column text \
-		--seq-len $seq_len \
-		--batch-size $batch \
-		--no-streaming \
-        	--model-config configs/$hnet.json > train_output.$hnet.txt
+			--seq-len $seq_len \
+			--batch-size $batch \
+			--no-streaming \
+        	--model-config configs/$hnet.json > output/train_output.$hnet.txt
 done
 kill $NVIDIA_SMI_PID
